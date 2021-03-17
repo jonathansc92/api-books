bc3f51fd48ea98b47e66dfa059e4a7148fef1b86
Jonathan Cruz <jonathansc92@gmail.com>
2021-03-17 04:09:04 -0300

    cors installed

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const app = express();
+const cors = require("cors");
+
+app.use(cors());
 
 const searchBooks = require('./googleBooksApi');
 const booklist = require('./booklist');
